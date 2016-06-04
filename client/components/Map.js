10597cf711637c52f8822adef2a47b50994680ae
Henry Kaufman <hkaufman19@gmail.com>
2016-06-04 11:57:04 -0600

    Base design done, may need a few fixes for efficiency

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,65 +1,91 @@
 import React from 'react';
-import makeMap, { getColorFromNumber } from '../vizualizations/map';
+import makeMap, {getColorFromNumber} from '../vizualizations/map';
 import _ from 'lodash';
 
 export default class Main extends React.Component {
-  componentDidMount() {
-    makeMap();
-  }
+    componentDidMount() {
+        makeMap();
+    }
 
-  render() {
-    const mapStyle = { width: '100%', height: '600px' };
-    const rangeMin = 0;
-    const rangeMax = 500;
-    const numberOfColors = 6;
+    render() {
+        const rangeMin = 0;
+        const rangeMax = 500;
+        const numberOfColors = 6;
 
-    const colors = _.range(numberOfColors).map(i => {
-      const percent = 100 / (numberOfColors - 1) * i;
-      const value = rangeMin + (rangeMax - rangeMin) / (numberOfColors - 1) * i;
-      const color = getColorFromNumber(value);
-      return `${color} ${percent}%`;
-    });
+        const colors = _.range(numberOfColors).map(i => {
+            const percent = 100 / (numberOfColors - 1) * i;
+            const value = rangeMin + (rangeMax - rangeMin) / (numberOfColors - 1) * i;
+            const color = getColorFromNumber(value);
+            return `${color} ${percent}%`;
+        });
 
-    const legendStyle = {
-      background: `linear-gradient(to right, ${colors.join(', ')})`
-    };
+        const legendStyle = {
+            background: `linear-gradient(to right, ${colors.join(', ')})`
+        };
 
-    return (
-      <div>
-        <h2>
-          Affordable Housing Units in the Denver Metro Area (2014)
-        </h2>
+        return (
+            <div>
 
-        <div id="map" style={ mapStyle } />
-
-        <div id="legend">
-          <h4>
-            Legend
-          </h4>
-          { rangeMin }
-          <span id="legendColorBar" style={ legendStyle } />
-          { rangeMax }
-          <div className="unit">
-            Affordable Units
-          </div>
-        </div>
-
-        <div>
-          Datasets:
-          <ul>
-            <li>
-              <a href="http://codataengine.org/find/2014-metro-denver-federally-subsidized-affordable-housing">
-                2014 Metro Denver Federally Subsidized Affordable Housing
-              </a>
-            </li>
-            <li>
-              <a href="https://data.colorado.gov/Demographics/Census-Blocks-2010/xipb-k5bu">
-                Census Blocks 2010
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+                <div id="map" className="map"/>
+                <div className="side">
+                    <h2>AltHousing</h2>
+                    <div className='options'>
+                        <h3>What do you want to see?</h3>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Affordable Housing</span>
+                        </div>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Safe neighborhoods</span>
+                        </div>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Quality schools</span>
+                        </div>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Public transit</span>
+                        </div>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Grocery (Deserts)</span>
+                        </div>
+                        <div className='box'>
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                <span></span>
+                            </label>
+                            <span className="checkbox-name">Areas for urban gardening</span>
+                        </div>
+                    </div>
+                </div>
+                {/* <div id="legend">
+                    <h4>
+                        Legend
+                    </h4>
+                    {rangeMin}
+                    <span id="legendColorBar" style={legendStyle}/> {rangeMax}
+                    <div className="unit">
+                        Affordable Units
+                    </div>
+                </div> */}
+            </div>
+        );
+    }
 }
